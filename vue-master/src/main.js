e61bb2c40b98aa3dcf15bdcdb9644f44f8005923
xiaolin <462441355@qq.com>
2020-06-23 16:04:08 +0800

    * 保存设备信息

--- vue-master/src/main.js
+++ vue-master/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Monitor from './utils/index'
+/* eslint-disable */
+let monitor = new Monitor()
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ vue-master/src/utils/index.js
@@ -0,0 +1,12 @@
+import Util from './util'
+
+let util = new Util()
+
+export default class Monitor {
+    constructor() {
+        console.log('sss')
+        util.getDevice()
+    }
+
+
+}
