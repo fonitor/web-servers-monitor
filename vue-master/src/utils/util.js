804bca448754ded836283ec00cedb09058b72605
xiaolin <462441355@qq.com>
2020-06-24 17:55:09 +0800

    * 增加一些url 解析

--- vue-master/src/utils/util.js
+++ vue-master/src/utils/util.js
@@ -154,6 +154,103 @@ export default class Util {
 
         return _type
     }
+
+    /**
+     * 判断数据
+     * @param {*} obj 
+     */
+    isBlank(obj) {
+        return obj === null || obj === undefined || obj === "";
+    }
+
+
+    /**
+     * 解析url中拼接的参数
+     * @param url
+     * @return {{参数名: string}}
+     */
+    parseUrlParams(url) {
+        if (url.indexOf("?") !== -1) {
+            url = url.substr(url.indexOf("?") + 1);
+        }
+        let paramsArr = url.match(/[^\?\=\&]*\=[^\?\=\&]*/g);
+        let params = {};
+        if (paramsArr != null) {
+            paramsArr.forEach(item => {
+                let kv = item.split("=");
+                params[decodeURIComponent(kv[0])] = decodeURIComponent(kv[1]);
+            });
+        }
+        return params;
+    }
+
+    /**
+     * 解析url
+     * @param {*} url 
+     */
+    getQueryObject(url) {
+        let search = url.substring(url.lastIndexOf("?") + 1),
+            obj = {},
+            reg = /([^?&=]+)=([^?&=]*)/g,
+            base_url = url.split("?")[0]
+        search.replace(reg, (rs, $1, $2) => {
+            let name = decodeURIComponent($1),
+                val = decodeURIComponent($2);
+            val = String(val);
+            obj[name] = val;
+        })
+
+        return {
+            baseUrl: base_url,
+            query: obj
+        }
+    }
+
+    /**
+   * 组装url 参数
+   * @param {*} queryObject 
+   */
+    makeQuery(queryObject) {
+        let query = Object.entries(queryObject)
+            .reduce((result, entry) => {
+                result.push(entry.join('='))
+                return result
+            }, []).join('&')
+        return `?${query}`
+    }
+
+    /**
+     * 删除url 指定参数
+     * @param {*} name 
+     */
+    funcUrlDel(name) {
+        var loca = location;
+        var baseUrl = loca.origin + loca.pathname + "?";
+        var query = loca.search.substr(1);
+        if (query.indexOf(name) > -1) {
+            var obj = {};
+            var arr = query.split("&");
+            for (var i = 0; i < arr.length; i++) {
+                arr[i] = arr[i].split("=");
+                obj[arr[i][0]] = arr[i][1];
+            }
+            delete obj[name];
+            var url = baseUrl + JSON.stringify(obj).replace(/[\"\{\}]/g, "").replace(/\:/g, "=").replace(/\,/g, "&");
+            return url
+        }
+    }
+
+    /**
+     * 修改url指定参数
+     * @param {*} paramName 
+     * @param {*} replaceWith 
+     */
+    replaceParamVal(paramName, replaceWith) {
+        var oUrl = location.href.toString();
+        var re = eval('/(' + paramName + '=)([^&]*)/gi');
+        location.href = oUrl.replace(re, paramName + '=' + replaceWith);
+        return location.href;
+    }
 }
 
 /* eslint-enable */
