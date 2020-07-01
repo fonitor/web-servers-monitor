39e0601a9c057c90917c2fbad008d84c4d449698
xiaolin <462441355@qq.com>
2020-07-01 10:00:03 +0800

    * add

--- vue-master/src/utils/config/index.js
+++ vue-master/src/utils/config/index.js
@@ -1,2 +1,3 @@
 export const JS_ERROR = 'js_error'
 export const RESOURCE_LOAD = 'resource_load'
+export const HTTP_LOG = 'http_log'
--- vue-master/src/utils/log/httpLog.js
+++ vue-master/src/utils/log/httpLog.js
@@ -78,9 +78,8 @@ export default class httpLog {
         window.addEventListener('ajaxLoadEnd', (e) => {
             /** @type {number} */
             let i = 0;
-            console.log(cache)
             for (i; i < cache.length; i++) {
-                if (true !== cache[i].uploadFlag) continue
+                if (true === cache[i].uploadFlag) continue
                 if (cache[i].event.detail.status > 0) {
                     if ("blob" === (cache[i].event.detail.responseType + "").toLowerCase()) {
                         // 闭包
@@ -123,33 +122,6 @@ export default class httpLog {
                     }
                 }
             }
-            // console.log(this.timeRecordArray)
-            // for (i; i < this.timeRecordArray.length; i++) {
-            //     // uploadFlag == true 代表这个请求已经被上传过了
-            //     if (this.timeRecordArray[i].uploadFlag === true) continue
-            //     console.log(this.timeRecordArray[i].event.detail.status)
-            //     if (this.timeRecordArray[i].event.detail.status > 0) {
-            //         let rType = (this.timeRecordArray[i].event.detail.responseType + "").toLowerCase()
-            //         console.log(rType)
-            //         if (rType === "blob") {
-            //             (function (index) {
-            //                 let reader = new FileReader()
-            //                 reader.onload = function () {
-            //                     let responseText = reader.result //内容就在这里
-            //                     this.handleHttpResult(index, responseText)
-            //                 }
-            //                 try {
-            //                     reader.readAsText(this.timeRecordArray[i].event.detail.response, 'utf-8')
-            //                 } catch (e) {
-            //                     this.handleHttpResult(index, this.timeRecordArray[i].event.detail.response + "")
-            //                 }
-            //             })(i)
-            //         } else {
-            //             let responseText = this.timeRecordArray[i].event.detail.responseText
-            //             this.handleHttpResult(i, responseText)
-            //         }
-            //     }
-            // }
         })
 
         this.fetch()
@@ -164,31 +136,32 @@ export default class httpLog {
      * @param {*} tempResponseText 
      */
     handleHttpResult(i, tempResponseText) {
-        console.log(i)
-        console.log(tempResponseText)
-        // let timeRecordArray = (util.isBlank(this.timeRecordArray) && util.isType().isArray(this.timeRecordArray)) ? this.timeRecordArray : []
-        // if (!timeRecordArray[i] || timeRecordArray[i].uploadFlag === true) {
-        //     return
-        // }
-        // let responseText = ""
-        // try {
-        //     responseText = tempResponseText ? JSON.stringify(util.encryptObj(JSON.parse(tempResponseText))) : ""
-        // } catch (e) {
-        //     responseText = ""
-        // }
-        // let simpleUrl = timeRecordArray[i].simpleUrl,
-        //     currentTime = new Date().getTime(),
-        //     url = timeRecordArray[i].event.detail.responseURL,
-        //     status = timeRecordArray[i].event.detail.status,
-        //     statusText = timeRecordArray[i].event.detail.statusText,
-        //     loadTime = currentTime - timeRecordArray[i].timeStamp;
-        // if (!url || url.indexOf(HTTP_UPLOAD_LOG_API) != -1) return
-        // let httpLogInfoStart = new HttpLogInfo(HTTP_LOG, simpleUrl, url, status, statusText, "发起请求", "", timeRecordArray[i].timeStamp, 0)
-        // httpLogInfoStart.handleLogInfo(HTTP_LOG, httpLogInfoStart);
-        // let httpLogInfoEnd = new HttpLogInfo(HTTP_LOG, simpleUrl, url, status, statusText, "请求返回", responseText, currentTime, loadTime)
-        // httpLogInfoEnd.handleLogInfo(HTTP_LOG, httpLogInfoEnd);
+        if (!cache[i] || cache[i].uploadFlag === true) {
+            return;
+        }
+        let responseText = ""
+        try {
+            responseText = tempResponseText ? JSON.stringify(util.encryptObj(JSON.parse(tempResponseText))) : ""
+        } catch (e) {
+            responseText = ""
+        }
+        let simpleUrl = cache[i].simpleUrl,
+            currentTime = new Date().getTime(),
+            url = cache[i].event.detail.responseURL,
+            status = cache[i].event.detail.status,
+            statusText = cache[i].event.detail.statusText,
+            loadTime = currentTime - cache[i].timeStamp;
+
+        if (!url) return
+        console.log('sss')
+        let httpLogInfoStart = this.httpLogInfo(error.HTTP_LOG, simpleUrl, url, status, statusText, "发起请求", "", timeRecordArray[i].timeStamp, 0)
+        console.log(httpLogInfoStart)
+        // httpLogInfoStart.handleLogInfo(error.HTTP_LOG, httpLogInfoStart)
+        let httpLogInfoEnd = this.httpLogInfo(error.HTTP_LOG, simpleUrl, url, status, statusText, "请求返回", responseText, currentTime, loadTime)
+        // httpLogInfoEnd.handleLogInfo(error.HTTP_LOG, httpLogInfoEnd)
         // 当前请求成功后就，就将该对象的uploadFlag设置为true, 代表已经上传了
-        // timeRecordArray[i].uploadFlag = true
+        cache[i].uploadFlag = true
+        console.log(cache)
     }
 
     /**
@@ -203,6 +176,7 @@ export default class httpLog {
      */
     httpLogInfo(uploadType, url, status, statusText, statusResult, currentTime, loadTime) {
         let obj = JSON.parse(JSON.stringify(util.getCommonProperty()))
+        console.log(obj)
         obj.uploadType = uploadType  // 上传类型
         obj.httpUrl = util.b64EncodeUnicode(encodeURIComponent(url)) // 请求地址
         obj.status = status // 接口状态
@@ -210,6 +184,8 @@ export default class httpLog {
         obj.statusResult = statusResult // 区分发起和返回状态
         obj.happenTime = currentTime  // 客户端发送时间
         obj.loadTime = loadTime // 接口请求耗时
+        console.log(obj)
+        return obj
     }
 
     /**
