fb0b06a935d09c4ca400d6ac7d44aab199fd1080
xiaolin <462441355@qq.com>
2020-08-11 10:17:51 +0800

    * mq 封装

--- src/library/mq/index.js
+++ src/library/mq/index.js
@@ -13,10 +13,37 @@ export default class RabbitMq {
      * 消息生产者
      * @param {*} queueName 
      * @param {*} msg 
-     * @param {*} errCallBack 
+     * @param {*} successCallback 
+     * @param {*} errorCallBack 
      */
-    sendQueueMsg(queueName, msg, errCallBack) {
-        
+    sendQueueMsg(queueName, msg, successCallback, errorCallBack) {
+        let self = this;
+        self.open
+            .then(function (conn) {
+                return conn.createChannel()
+            })
+            .then(function (channel) {
+                return channel.assertQueue(queueName).then(function (ok) {
+                    return channel.sendToQueue(queueName, new Buffer.from(msg), {
+                        persistent: true
+                    })
+                }).then(function (data) {
+                    if (data) {
+                        typeof successCallback === "function" && successCallback("success")
+                        channel.close()
+                    }
+                }).catch(function (e) {
+                    errorCallBack ** errorCallBack(e)
+                    setTimeout(() => {
+                        if (channel) {
+                            channel.close()
+                        }
+                    }, 500)
+                })
+            })
+            .catch(function (e) {
+                typeof errorCallBack === "function" && errorCallBack(e)
+            })
     }
 
     /**
@@ -26,6 +53,38 @@ export default class RabbitMq {
      * @param {*} errCallBack 
      */
     receiveQueueMsg(queueName, receiveCallBack, errCallBack) {
+        let self = this
 
+        self.open
+            .then(function (conn) {
+                return conn.createChannel()
+            })
+            .then(function (channel) {
+                return channel.assertQueue(queueName).then(function (ok) {
+                    channel.prefetch(10, false)
+                    return channel.consume(queueName, function (msg) {
+                        if (msg !== null) {
+                            let data = msg.content.toString()
+                            receiveCallBack && receiveCallBack(data, function () { })
+                            channel.ack(msg)
+                        }
+                    }).finally(function () { })
+                })
+            })
+            .catch(function (e) {
+                errCallBack(e)
+                /**
+                 * 下面的逻辑是做容灾处理，会有多个rabbitmq服务用来切换
+                 * @type {number}
+                  let num = self.index++;
+                   if (num <= self.length - 1) {
+                    self.open = amqp.connect(self.hosts[num]);
+                  } else {
+                    self.index = 0;
+                    self.open = amqp.connect(self.hosts[0]);
+                  }
+                 */
+
+            })
     }
 }
