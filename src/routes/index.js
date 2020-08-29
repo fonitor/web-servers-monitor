f440717ef2706128518aebd8da3efa88f0723de7
xiaolin <462441355@qq.com>
2020-08-29 22:55:24 +0800

    * 自动注册api 路由

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,41 @@
 var express = require('express')
 var router = express.Router()
 
+// 路由分为以下部分
+// 不需要登录
+const withoutLoginRouter = express.Router()
+
+/**
+ * 根据请求方法注册路由
+ * @param {*} customerRouter
+ * @param {*} routerConfig
+ * @param {*} url
+ */
+function registerRouterByMethod(customerRouter, routerConfig, url) {
+  switch (routerConfig.methodType) {
+    case RouterConfigBuilder.METHOD_TYPE_GET:
+      customerRouter.get(url, (req, res) => {
+        return routerConfig.func(req, res)
+      })
+      break
+    case RouterConfigBuilder.METHOD_TYPE_POST:
+      customerRouter.post(url, (req, res) => {
+        return routerConfig.func(req, res)
+      })
+      break
+    default:
+  }
+}
+
+// 自动注册
+for (let url of Object.keys(routerConfigMap)) {
+  let routerConfig = routerConfigMap[url]
+  // 不需要登录
+  registerRouterByMethod(withoutLoginRouter, routerConfig, url)
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send({ title: '测试返回' })
 })
 
