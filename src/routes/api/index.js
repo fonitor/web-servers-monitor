b79acdcef6b3d956423e2a474e8c2d390bdcd0ad
xiaolin <462441355@qq.com>
2021-01-28 21:10:44 +0800

    * 初始化页面

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,10 +2,12 @@ import saveLog from './log'
 import js from './error/js'
 import http from './http/index'
 import project from './project/index'
+import page from './page/index'
 
 export default {
     ...saveLog,
     ...js,
     ...http,
-    ...project
+    ...project,
+    ...page
 }
